0bb7f5d47c10b6ce80199add738c8945afd13111
rzabka1 <jturovska@gmail.com>
2025-03-26 23:20:10 +0100

    Initial fixes
    Switched chair tilemap to scene collection, minor code polish

--- Scripts/wagon.gd
+++ Scripts/wagon.gd
@@ -1,23 +1,29 @@
 extends Node2D
 
+@onready var chairs = $YSort/Chairs
+
+
 func _ready() -> void:
 	spawn_passengers()
 
 func spawn_passengers():
-	var cells_ac = $YSort/ChairsAC.get_used_cells()
-	var cells_bd = $YSort/ChairsBD.get_used_cells()
-	var cells = cells_ac + cells_bd ## an array of all chair coords
+	var cells:Array = chairs.get_used_cells() # an array of all chair coords
 	cells.shuffle()
 	
-	var num_of_passengers = randi_range(11,12)
-	var spawn_count:int = 0 ## used for breaking the loop
+	var min_passengers:int = 4
+	var max_passengers:int = 12
+	var num_of_passengers:int = randi_range(min_passengers, max_passengers)
+	var instance_count:int = 0 # used for breaking the loop
 	
 	for cell in cells:
-		var tile_pos = $YSort/ChairsAC.map_to_local(cell)
+		# converting tilemap coords to scene coords
+		var tile_pos = chairs.map_to_local(cell)
+		# Instancing passengers
 		var passenger_scene = load("res://Scenes/passenger.tscn")
 		var passenger = passenger_scene.instantiate()
 		$YSort/Passengers.add_child(passenger)
-		passenger.position = (tile_pos-Vector2(-4,5))*2
-		spawn_count +=1
-		if spawn_count == num_of_passengers:
+		passenger.position = tile_pos * chairs.scale
+		# checking if all passengers are instanced
+		instance_count +=1
+		if instance_count == num_of_passengers:
 			break
