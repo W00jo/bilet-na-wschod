c23fbfb1a703839b8c038ef05e2aaf8258024701
Jakub <94285273+W00jo@users.noreply.github.com>
2025-06-19 20:36:07 +0200

    Further testing of dialogue options
    + buttons are one-time pressable only
    + dialogue shows up properly, according to NPCs personality and players chosen dialogue option/command button.

--- Scripts/test_dialogue.gd
+++ Scripts/test_dialogue.gd
@@ -5,7 +5,7 @@ var dialogue = {
 		"greetings": ["Dzień dobry!", "Czołem Panie konduktorze!"],
 		"ticket_yes": ["Proszę oto bilet, proszę Pana."],
 		"ticket_no": ["O nie... chyba mi wypadł..."],
-		"id_yes": ["Proszę bardzo. [P] Wszystko się zgadza, mam nadzieję?"],
+		"id_yes": ["Proszę bardzo. Wszystko się zgadza, mam nadzieję?"],
 		"id_no": ["Chyba został w innych spodniach..."]
 	},
 	"rude": {
@@ -26,26 +26,30 @@ var dialogue = {
 
 var dialogue_box: RichTextLabel
 var personality: String
+var bilet_button: Button
+var id_button: Button
 
 func _ready():
 	dialogue_box = $Panel/RichTextLabel
-	# Przy starcie sceny powinno wybierać losowo osbowość :3
+	bilet_button = $VBoxContainer/Bilet
+	id_button = $VBoxContainer/Id
 	var personalities = ["polite", "rude", "fraidy"]
 	personality = personalities.pick_random()
-	# "greetings" używa tylko na starcie
 	var greeting = dialogue[personality]["greetings"].pick_random()
-	dialogue_box.text = "[left]" + greeting + "[/left]"
+	dialogue_box.text = "[left]%s[/left]" % greeting
 
 func _on_bilet_pressed():
 	var yes_or_no = ["ticket_yes", "ticket_no"].pick_random()
 	var npc_text = dialogue[personality][yes_or_no].pick_random()
 	var player_text = "Proszę bilet"
-	dialogue_box.text = "[left]%s[/left]\n[right]%s[/right]" % [npc_text, player_text]
+	dialogue_box.text += "\n[right]%s[/right]\n[left]%s[/left]" % [player_text, npc_text]
+	bilet_button.disabled = true
 	print("Bilet pressed, personality:", personality, "key:", yes_or_no, "npc:", npc_text)
 
 func _on_id_pressed():
 	var yes_or_no = ["id_yes", "id_no"].pick_random()
 	var npc_text = dialogue[personality][yes_or_no].pick_random()
-	var player_text = "Proszę bilet"
-	dialogue_box.text = "[left]%s[/left]\n[right]%s[/right]" % [npc_text, player_text]
-	print("Bilet pressed, personality:", personality, "key:", yes_or_no, "npc:", npc_text)
+	var player_text = "Proszę się wylegitymować"
+	dialogue_box.text += "\n[right]%s[/right]\n[left]%s[/left]" % [player_text, npc_text]
+	id_button.disabled = true
+	print("ID pressed, personality:", personality, "key:", yes_or_no, "npc:", npc_text)
