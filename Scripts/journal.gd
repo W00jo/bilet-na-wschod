1fb3bc71490befa90399017b429ae55558178878
Jakub <94285273+W00jo@users.noreply.github.com>
2025-07-06 22:34:35 +0200

    Add page turn sound effect to journal navigation
    Introduced an AudioStreamPlayer to play a page turn sound when navigating between journal and credits pages.

--- Scripts/journal.gd
+++ Scripts/journal.gd
@@ -1,8 +1,13 @@
 extends Control
 
-
+@onready var page_sound = AudioStreamPlayer.new()
 
 func _ready() -> void:
+	# Add and configure the audio player
+	add_child(page_sound)
+	page_sound.stream = load("res://Assets/SFX/page.wav")
+	page_sound.volume_db = -10  # Adjust volume as needed
+	
 	if get_parent() == get_tree().root.get_node('Game/ToolkitLayer/Toolkit'):
 		$CreditsPages.visible = false
 		hide_all_journal_pages()
@@ -26,39 +31,47 @@ func hide_all_credits_pages():
 func _on_button_cover_pressed() -> void:
 	hide_all_journal_pages()
 	$JournalPages/Pages_1_2.visible = true
+	page_sound.play()
 
 func _on_button_page_1_pressed() -> void:
 	hide_all_journal_pages()
-	$JournalPages/Cover. visible = true
+	$JournalPages/Cover.visible = true
+	page_sound.play()
 
 func _on_button_page_2_pressed() -> void:
 	hide_all_journal_pages()
 	$JournalPages/Pages_3_4.visible = true
+	page_sound.play()
 
 func _on_button_page_3_pressed() -> void:
 	hide_all_journal_pages()
 	$JournalPages/Pages_1_2.visible = true
+	page_sound.play()
 
 ###############################################
 
 func _on_credits_cover_pressed() -> void:
 	hide_all_credits_pages()
 	$CreditsPages/Pages_1_2.visible = true
+	page_sound.play()
 
 func _on_credits_page_1_pressed() -> void:
 	hide_all_credits_pages()
 	$CreditsPages/Cover.visible = true
+	page_sound.play()
 
 func _on_credits_page_2_pressed() -> void:
 	hide_all_credits_pages()
 	$CreditsPages/Pages_3_4.visible = true
+	page_sound.play()
 
 func _on_credits_page_3_pressed() -> void:
 	hide_all_credits_pages()
 	$CreditsPages/Pages_1_2.visible = true
+	page_sound.play()
 
 ###############################################
 
-func _input(event: InputEvent) -> void:
+func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Magnify"):
 		queue_free()
