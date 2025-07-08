e78c3007f6e17554fcef5e9419ee7178856c0235
rzabka1 <jturovska@gmail.com>
2025-07-08 22:26:34 +0200

    Revert "Journal"
    This reverts commit 0043cf7cc216d209a563d0ecafa39770803945db.

--- Scripts/journal.gd
+++ Scripts/journal.gd
@@ -1,8 +1,13 @@
 extends Control
 
-@onready var page_sound = $PageSFX
+@onready var page_sound = AudioStreamPlayer.new()
 
 func _ready() -> void:
+	# Add and configure the audio player
+	add_child(page_sound)
+	page_sound.stream = load("res://Assets/SFX/page.wav")
+	page_sound.volume_db = -10  # Adjust volume as needed
+	
 	if get_parent() == get_tree().root.get_node('Game/ToolkitLayer/Toolkit'):
 		$CreditsPages.visible = false
 		hide_all_journal_pages()
@@ -46,7 +51,6 @@ func _on_button_page_3_pressed() -> void:
 ###############################################
 
 func _on_credits_cover_pressed() -> void:
-	print("PRESSED")
 	hide_all_credits_pages()
 	$CreditsPages/Pages_1_2.visible = true
 	page_sound.play()
@@ -71,11 +75,3 @@ func _on_credits_page_3_pressed() -> void:
 func _input(_event: InputEvent) -> void:
 	if Input.is_action_just_pressed("Magnify"):
 		queue_free()
-
-
-func _on_button_page_4_pressed() -> void:
-	pass # Replace with function body.
-
-
-func _on_credits_page_4_pressed() -> void:
-	pass # Replace with function body.
